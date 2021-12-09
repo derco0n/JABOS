6160c18c9b5118b5f09b5a9102335582cdf98a5d
derco0n <dennis_marx@web.de>
2021-12-09 20:27:25 +0100

    added linux firefox CA import script

---
+++ BASH/import-ca-cert-to-firefox-on-linux.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+### Script installs root.cert.pem to certificate trust store of applications using NSS
+### (e.g. Firefox, Thunderbird, Chromium)
+### Mozilla uses cert8, Chromium and Chrome use cert9
+
+###
+### Requirement: apt install libnss3-tools
+###
+
+
+###
+### CA file to install (CUSTOMIZE!)
+###
+function printhelp {
+    echo ""
+    echo "$0 - imports CA_certificates into firefox"    
+    echo "Usage: $0 <certfile> <certname>"
+    echo ""
+    echo "certfile - file that contains a CA-certificate"
+    echo "certname - name for the certificate to import"
+}
+
+if [ -z "$1" ]; then 
+    echo "certfile not given";
+    printhelp;
+    exit 1;
+fi
+
+if [ -z "$2" ]; then 
+    echo "certname not given";
+    printhelp;
+    exit 2;
+fi
+
+
+
+certfile=$1
+certname=$2
+
+
+###
+### For cert8 (legacy - DBM)
+###
+
+for certDB in $(find ~/ -name "cert8.db")
+do
+    certdir=$(dirname ${certDB});
+    certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d dbm:${certdir}
+done
+
+
+###
+### For cert9 (SQL)
+###
+
+for certDB in $(find ~/ -name "cert9.db")
+do
+    certdir=$(dirname ${certDB});
+    certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d sql:${certdir}
+done
